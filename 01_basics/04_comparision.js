5b0a43808f1201cb062c223496f3f2c369050666
Rahul Shukla <shuklarahulkanpur@gmail.com>
2026-07-14 05:20:06 +0530

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(6>5);  // true
+// console.log(6>=5)  // true
+// console.log(6<5)   //false
+// console.log(6==5)   //false
+// console.log(6 != 5)  // true
+
+
+// console.log("7" > 1)
+// console.log("08" >1)
+
+console.log(null > 0)         //avoid this type comparisions
+console.log(null == 0)         //avoid this type comparisions
+console.log(null >= 0)         //avoid this type comparisions
+
+console.log(undefined == 0)         //avoid this type comparisions
+console.log(undefined > 0)         //avoid this type comparisions
+console.log(undefined < 0)         //avoid this type comparisions
+
+// === strict check
+
+console.log("2" === 2)
+
