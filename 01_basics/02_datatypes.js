ea67719c053d7b8a72b63a634a68faf9e0fb7ce6
Rahul Shukla <shuklarahulkanpur@gmail.com>
2026-07-14 02:29:21 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+//DataTypes--
+"use strict"; //treal all JS code as newer version
+
+// alert(2+2) // using Node.js not Browser 
+console.log(3+3);  //code readibility needes to be high
+console.log("rahul"); //code readibility needes to be high
+
+let name = "rahul"
+let age = 24
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint -trading etc.
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof "rahul") //string
+console.log(typeof age) //number
+console.log(typeof null) //-object
+console.log(typeof undefined) //undefined
