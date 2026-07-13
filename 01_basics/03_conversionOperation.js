af201b00085e23f674ea89a48291b7ac33006371
Rahul Shukla <shuklarahulkanpur@gmail.com>
2026-07-14 02:47:59 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "69"
+// let score = "69abc"
+// let score = null
+let score = "rahul"
+
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); //Number
+console.log(valueInNumber) //NaN
+console.log(valueInNumber) // 0
+console.log(valueInNumber)  //NaN
+
+// "69" => 69
+// "69abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//true => 1; false => 
+// ""=> false
+// "rahul" => true
+
+ someNumber = 69
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
