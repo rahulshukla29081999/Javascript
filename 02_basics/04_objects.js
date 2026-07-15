88c1dc5a5f7cdda94408d553daa795ea583206c1
Rahul Shukla <shuklarahulkanpur@gmail.com>
2026-07-15 13:57:19 +0530

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,5 +24,61 @@
 const obj2 = {3: "a", 4: "b"}
 
 // const obj3 = {obj1, obj2} 
-const obj3 = Object.assign({}, obj1,obj2)
+// const obj3 = Object.assign({}, obj1,obj2)
+const obj3 = {...obj1, ...obj2}               // spread operator
 console.log(obj3)
+
+const users = [
+    {
+        id : 1,
+        email : "rahul@gmail.com",
+    },
+    {
+         id :2 ,
+        email : "rahu@gmail.com",
+    }
+]
+console.log(users[1].email)
+
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser))                // [ 'Id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser))              // [ '123abc', 'Sammy', false ]
+console.log(Object.entries(tinderUser))             // [ [ 'Id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ] 
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))         // true
+
+
+
+/*++++++++++Object Destructuring+++++++++*/
+
+// Object  ko toh Destructuring kiya hi jata h aur Array ko bhi kar skte hain
+
+
+const course = {
+    coursename: "JavaScript",
+    price: "999",
+    CourseInstructor: "Hitesh"
+
+}
+
+// course.coursename
+
+const{coursename:c_name} = course
+console.log(c_name)
+
+// {
+//     "name": "rahul",
+//     "coursename": " JavaScript",
+//     "price": "free"
+
+// }                                 // JSON --- API
+
+
+// [
+
+// {},
+// {},                               //  JSON - ARRAY Format - API
+// {}
+// ]
+
