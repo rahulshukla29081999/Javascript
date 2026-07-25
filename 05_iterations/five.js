bf8fefde6b8c9ea8677ebfb58d7f20f47ce0beea
Rahul Shukla <shuklarahulkanpur@gmail.com>
2026-07-25 22:03:55 +0530

    five.js

---
+++ 05_iterations/five.js
@@ -0,0 +1,12 @@
+const coding = ["js","cpp","python","ruby"]
+
+// coding.forEach( function (item){     
+// // js
+// // cpp
+// // python
+// // ruby
+//   console.log(item)  
+// })                               
+coding.forEach((item) => {
+    console.log(item)
+})
